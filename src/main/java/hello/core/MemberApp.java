8a151ecb2424fbe6500564779f5d8d3140845285
lim4158 <lim4158@gmail.com>
2023-12-15 13:47:25 +0900

    [Update] 주석추가

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -16,6 +16,7 @@ public class MemberApp {
 //        MemberService memberService = appConfig.memberService(); //AppConfig에서 가져와 사용. 의존관계 주입
 //        MemberService memberService = new MemberServiceImpl(); -> 직접생성
 
+        //Spring Container
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
 
         // DI 컨테이너에 등록된 Bean은 기본적으로 메소드 명으로 지정된다.
